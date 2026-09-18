4532780157500649b6dafd2c1aada2f8c59a35c6
Gaurav Kumar Rawal <gauravkumarrawal99@gmail.com>
2026-09-18 14:16:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,24 @@ document.querySelectorAll('.nav-links a').forEach(link => {
     });
 });
 // ====================== CERTIFICATE MODAL ======================
+// ====================== CERTIFICATE MODAL ======================
 function openCertificate(type) {
     const modal = document.getElementById('certificateModal');
     const frame = document.getElementById('certificateFrame');
 
+    let file = "";
+
     if (type === 'excel') {
-        frame.src = 'certificates/excel.pdf';
+        file = 'certificates/excel.pdf';
     } 
     else if (type === 'canva') {
-        frame.src = 'certificates/canva.pdf';
+        file = 'certificates/canva.pdf';
     } 
     else if (type === 'ai-era') {
-        frame.src = 'certificates/ai-era.pdf';
+        file = 'certificates/ai-era.pdf';
     }
 
+    frame.src = file;
     modal.style.display = 'block';
 }
 
@@ -33,7 +37,7 @@ function closeCertificate() {
     const frame = document.getElementById('certificateFrame');
     
     modal.style.display = 'none';
-    frame.src = ''; // PDF band karne ke liye
+    frame.src = ''; 
 }
 
 // Modal ke bahar click karne pe band ho jaye
